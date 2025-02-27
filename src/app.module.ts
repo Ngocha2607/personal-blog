f09f2a3761923b43710cae29b0b41542a9b79ad9
Ngocha2607 <ngocha2k.ln@gmail.com>
2025-02-27 23:00:05 +0700

    add validation and new dependencies for blog module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,17 @@
-import { Module } from '@nestjs/common';
+import { Module, ValidationPipe } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BlogController } from './blog/blog.controller';
 import { ConfigModule } from '@nestjs/config';
 import { BlogService } from './blog/blog.service';
 import { BlogModule } from './blog/blog.module';
+import { APP_PIPE } from '@nestjs/core';
 @Module({
   imports: [ConfigModule.forRoot({ isGlobal: true }), BlogModule],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [{
+    provide: APP_PIPE,
+    useClass: ValidationPipe,
+  }],
 })
 export class AppModule {}
